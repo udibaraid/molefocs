3aa5635ae288c4f6245d9f7a46f03e495fbf2d76
Budi Santoso <budisantbs7@gmail.com>
2022-10-27 15:21:51 +0700

    audio_lib

--- lib/model/listening_model.dart
+++ lib/model/listening_model.dart
@@ -2,11 +2,13 @@ class Listening {
   final String name;
   final String image;
   final String desc;
+  final String audio;
 
   Listening({
     required this.name,
     required this.image,
     required this.desc,
+    required this.audio,
   });
 }
 
@@ -15,35 +17,42 @@ List<Listening> lst = [
     name: "Introduce Yourself to Digital World!",
     image: "assets/images/img_speaking1.jpg",
     desc: "assets/pdf/listening1.pdf",
+    audio: "assets/audio/unit_1",
   ),
   Listening(
     name: "Go Beyond Computer Essentials",
     image: "assets/images/img_speaking2.jpg",
     desc: "assets/pdf/listening2.pdf",
+    audio: "assets/audio/unit_2",
   ),
   Listening(
     name: "Inside the System",
     image: "assets/images/img_speaking3.jpg",
     desc: "assets/pdf/listening3.pdf",
+    audio: "assets/audio/unit_3",
   ),
   Listening(
     name: "You Must Know Basic Software",
     image: "assets/images/img_speaking4.jpg",
     desc: "assets/pdf/listening4.pdf",
+    audio: "assets/audio/unit_4",
   ),
   Listening(
     name: "Keep In Touch with The Internet",
     image: "assets/images/img_speaking5.jpg",
     desc: "assets/pdf/listening5.pdf",
+    audio: "assets/audio/unit_5",
   ),
   Listening(
     name: "Network",
     image: "assets/images/img_speaking6.jpg",
     desc: "assets/pdf/listening6.pdf",
+    audio: "assets/audio/unit_6",
   ),
   Listening(
     name: "Let’s go to Bright Future in IT Jobs",
     image: "assets/images/img_speaking7.jpg",
     desc: "assets/pdf/listening7.pdf",
+    audio: "assets/audio/unit_7",
   ),
 ];
